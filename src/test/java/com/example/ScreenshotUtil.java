61550a3d8c439a80fc6b43ede3ad708d1cdbbb82
sreegithub19 <cpadmaja2003@gmail.com>
2025-06-12 19:52:06 +0530

    C

--- src/test/java/com/example/ScreenshotUtil.java
+++ src/test/java/com/example/ScreenshotUtil.java
@@ -10,6 +10,10 @@ public class ScreenshotUtil {
 
     public static void takeScreenshot(AndroidDriver driver, String fileName) {
         try {
+            File screenshotDir = new File("screenshots");
+                if (!screenshotDir.exists()) {
+                    screenshotDir.mkdirs();
+                }
             File srcFile = driver.getScreenshotAs(OutputType.FILE);
             FileUtils.copyFile(srcFile, new File("screenshots/" + fileName + ".png"));
         } catch (Exception e) {
